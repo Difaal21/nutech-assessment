c5632dc8f643c4aedaa8ff3ce0694636053e1eb6
Difa Al Fansha <difaal21@gmail.com>
2024-12-12 14:21:05 +0700

    middleware: add log in file uploader

--- middlewares/file_uploader.js
+++ middlewares/file_uploader.js
@@ -3,6 +3,7 @@ import multer from "multer";
 import path from "path";
 import { fileURLToPath } from 'url';
 import { v7 as uuidv7 } from 'uuid';
+import logger from "../helpers/utils/logger.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -27,12 +28,16 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({ storage, fileFilter, limits: { fileSize: 2 * 1024 * 1024 } }).single('file');
 
 const fileUploader = (req, res, next) => {
+  const ctx = "middlewares.fileUploader"
   upload(req, res, (err) => {
     if (err instanceof multer.MulterError) {
+      logger.log(ctx, err.message, 'instanceof multer.MulterError')
       return new httpResponse.BadRequest().setMessage("Format Image tidak sesuai").send(res);
     } else if (err) {
-      return new httpResponse.BadRequest().setMessage("Format Image tidak sesuai").send(res);
+      logger.log(ctx, err.message, 'multer.err')
+      return new httpResponse.InternalServerError().setMessage("Terjadi kesalahan yang tidak diketahui").send(res);
     } else if (!req.file) {
+      logger.log(ctx, "file tidak ada", '!req.file')
       return new httpResponse.BadRequest().setMessage("Format Image tidak sesuai").send(res);
     }
     next()
